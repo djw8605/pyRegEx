312619dda07696da2bf40693e3a1d2c3c1701bdd
Derek Weitzel <djw8605@gmail.com>
2011-10-09 20:38:37 -0500

    Removing unnecessary code.

--- src/pyRegEx.py
+++ src/pyRegEx.py
@@ -18,78 +18,7 @@ class NFANode:
         pass
     def __str__(self):
         return str(self.id)
-    #def __str__(self):
-        #print self.transition
-        #for key in self.transition.keys():
-        #    print key + "->" + str(self.transition[key])
-    
 
-class NFAGroup(NFANode):
-    def __init__(self):
-        pass
-    
-
-NFAGraph = []
-
-class Tree:
-    def __init__(self, cargo, left=None, right=None):
-        self.cargo = cargo
-        self.left  = left
-        self.right = right
-
-    def __str__(self):
-        string = ""
-        if self.left is not None:
-            string += str(self.left)
-        string += str(self.cargo)
-        if self.right is not None:
-            string += str(self.right)
-            
-        return string
-
-
-def get_token(token_list, expected):
-    if token_list[0] == expected:
-        del token_list[0]
-        return True
-    else:
-        return False
-    
-    
-def get_letter(token_list):
-    
-    if get_token(token_list, '('):
-        x = get_star(token_list)
-        get_token(token_list, ')')
-        return x
-    else:
-        x = token_list[0]
-        if x != 'a' and x != 'b':
-            print x
-            print "returning none"
-            return None
-        token_list[0:1] = []
-        return Tree (x, None, None)    
-
-
-def get_or(token_list):
-    print token_list
-    a = get_letter(token_list)
-    print a
-    if get_token(token_list, "|"):
-        b = get_or(token_list)
-        print "Orring %s | %s" % (str(a), str(b))
-        return Tree('|', a, b)
-    else:
-        return a
-
-def get_star(token_list):
-    a = get_or(token_list)
-    if get_token(token_list, '*'):
-        print "Starring %s" % a
-        return Tree ('*', a, None)
-    else:
-        return a
 
 operators = ["|", "&"]
 
@@ -150,26 +79,8 @@ def scan(regex_string, start_index, stop_index, parenthese_level):
     start_node.transition[character] = final_node
     return (start_node, [final_node])
         
-    pass
 
 
-def create_graph(regex_string, current_index, first_node, final_nodes):
-    print "Reading %s" % regex_string[current_index]
-    if regex_string[current_index] == '(':
-        return create_graph(regex_string, current_index+1, first_node, final_nodes)
-    elif regex_string[current_index] == '|':
-        (front_node1, final_nodes1) = create_graph(regex_string, current_index+1, first_node, final_nodes)
-        new_front = NFANode()
-        new_front.transition["e"] = front_node1
-        new_front.transition["e"] = first_node
-        return (new_front, [final_nodes])
-        
-    elif regex_string[current_index] == 'a' or regex_string[current_index] == 'b':
-        first_node = NFANode()
-        last_node = NFANode()
-        first_node.transition[regex_string[current_index]] = last_node
-        return (first_node, [last_node])
-    
 
 seen_nodes = []
 def printtree(node):
@@ -209,25 +120,7 @@ def convert_regex(regex_string):
         print str(node) + " ",
     print final_nodes
     printtree(start_nodes)
-    
-    #print new_string
-    #graph_start = NFANode()
-    #print list(regex_string)
-    #token_list = list(regex_string.strip())
-    #master_tree = []
-    #while len(token_list) > 0:
-    #    print "Running tree"
-    #    tree = get_star(token_list)
-    #    master_tree.append(tree)
-    #graph = create_graph(regex_string, 0, None, None)
-    #print graph[0].transition
-    #print "tree:"
-    #print master_tree
-        
-            
-            
-            
-    pass
+
 
 
 def main():
